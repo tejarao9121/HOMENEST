08d306c55a12ab8bc124212d5252bba7d5712a6c
saiteja <tejarao9121@gmail.com>
2024-11-06 22:05:38 +0530

    updating

--- client/src/components/SearchMap.js
+++ client/src/components/SearchMap.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+// import React from 'react';
+// import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google, location }) => {
-  return (
-    <Map google={google} zoom={10} initialCenter={{ lat: 37.09024, lng: -95.712891 }}>
-      <Marker position={location} />
-    </Map>
-  );
-};
+// const MapContainer = ({ google, location }) => {
+//   return (
+//     <Map google={google} zoom={10} initialCenter={{ lat: 37.09024, lng: -95.712891 }}>
+//       <Marker position={location} />
+//     </Map>
+//   );
+// };
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyC_bYbyJo8vp-Dn2yVPP2p1DmS9US86_Ds'
-})(MapContainer);
+// export default GoogleApiWrapper({
+//   apiKey: 'AIzaSyC_bYbyJo8vp-Dn2yVPP2p1DmS9US86_Ds'
+// })(MapContainer);
 
